651f20fce1e86e92bd91ef550860c69ed09e137a
KaguyaKawaii <stephenpatingomadero@gmail.com>
2025-07-07 02:15:53 +0800

    Update 1.8.1

--- practice/src/Login/Login_User.jsx
+++ practice/src/Login/Login_User.jsx
@@ -58,7 +58,7 @@ function Login_User({ onSwitchToSignUp, onLoginSuccess, setView }) {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/users/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/users/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
