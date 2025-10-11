829cf8d30fa1183407d882954a9162d5fb9d0ac5
KaguyaKawaii <stephenpatingomadero@gmail.com>
2025-10-11 22:08:27 +0800

    Update SignUp_User.jsx

--- practice/src/Login/SignUp_User.jsx
+++ practice/src/Login/SignUp_User.jsx
@@ -559,7 +559,7 @@ const handleSubmit = async (e) => {
             </div>
             <h3 className="text-2xl font-bold text-gray-800">Registration Successful!</h3>
             <p className="mt-4 text-gray-600">
-              Your account is now active. Click below to log in and start
+              Your account is now active. Click below to log in and start okay
               reserving rooms.
             </p>
             <button
