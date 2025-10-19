3a0282a0c02ba7f66dc2d1484762585ab83ce2ad
KaguyaKawaii <stephenpatingomadero@gmail.com>
2025-10-19 19:11:37 +0800

    Update

--- .history/practice/vite.config_20250623175646.js
+++ .history/practice/vite.config_20250623175646.js
@@ -6,9 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss()
+
   ],
-  build: {
-    chunkSizeWarningLimit: 1600, // increase limit from default 500 KB to 1.6 MB
-  },
 })
